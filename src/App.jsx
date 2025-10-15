0a8879d972a95b9d78d207663e27cb61bd0a0237
Pratik <Pratikkakade4618@gmail.com>
2025-10-15 18:30:09 +0530

    Added privacy policy route

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import About from "./components/About";
@@ -6,7 +8,8 @@ import Learning from "./components/Learning";
 import Carousel from "./components/Carousel";
 import Registration from "./components/Registration";
 import Footer from "./components/Footer";
-import 'leaflet/dist/leaflet.css';
+import PrivacyPolicy from "./components/PrivacyPolicy";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
@@ -19,6 +22,11 @@ function App() {
       <Carousel />
       <Registration />
       <Footer />
+      <Router>
+        <Routes>
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
