9426a4e600e321013b37e22efd1637403a4e2527
Pratik <Pratikkakade4618@gmail.com>
2025-10-27 10:34:00 +0530

    Fixed the whatsapp message encoding issue

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -111,6 +111,104 @@ const Registration = () => {
     });
   };
 
+  const handleWhatsAppRedirect = () => {
+  const message = `🎶 ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴄᴏɴᴛᴀᴄᴛɪɴɢ 🎶
+            𝟕𝐭𝐡 𝐂𝐇𝐎𝐑𝐃𝐒 𝐌𝐔𝐒𝐈𝐂
+       & 𝐒𝐊𝐈𝐋𝐋𝐄𝐀𝐑𝐍 𝐀𝐂𝐀𝐃𝐄𝐌𝐘..!
+        👩🏻‍💻ʜᴏᴡ ᴡᴇ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ?
+
+* https://www.instagram.com/7th_chords_music_academy
+
+🕹️ 𝐒𝐭𝐚𝐲 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐎𝐮𝐫 𝐏𝐚𝐠𝐞'𝐬
+
+🌐 𝐆𝐨𝐨𝐠𝐥𝐞 𝐏𝐚𝐠𝐞 :-
+* https://g.co/kgs/Q1ZTyox
+
+🪀 𝐁𝐮𝐬𝐢𝐧𝐞𝐬𝐬 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 :- 9595618414
+
+📧 𝐆𝐦𝐚𝐢𝐥 :- www.7thchords@gmail.com
+
+🗺️ 𝐆𝐨𝐨𝐠𝐥𝐞 𝐌𝐚𝐩𝐬 𝐋𝐨𝐜𝐚𝐭𝐢𝐨𝐧 :-
+* https://maps.app.goo.gl/DiyPFxunk5xSRicE6
+
+📰 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 𝐏𝐚𝐠𝐞 :-
+* https://www.instagram.com/7th_chords_music_academy
+* https://www.instagram.com/aniket_ag7
+
+📲 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐏𝐚𝐠𝐞 :-
+* https://www.facebook.com/7thchordsm?sfnsn=wa
+* https://www.facebook.com/7thChordsM.A?sfnsn=wa
+
+▶️ 𝐘𝐨𝐮 𝐓𝐮𝐛𝐞 :-
+- https://youtube.com/@7thchordsbandmusicacademy?si=drum3v0rZ0KD3HZX
+
+❓𝐅𝐫𝐞𝐪𝐮𝐞𝐧𝐭𝐥𝐲 𝐀𝐬𝐤𝐞𝐝 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 :-
+
+🚏 𝐇𝐨𝐩𝐞 𝐲𝐨𝐮 𝐡𝐚𝐯𝐞 𝐚 𝐠𝐫𝐞𝐚𝐭 𝐞𝐱𝐩𝐞𝐫𝐢𝐞𝐧𝐜𝐞 𝐭𝐨 𝐤𝐧𝐨𝐰 𝐚𝐛𝐨𝐮𝐭 𝐮𝐬 ...
+𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐲𝐨𝐮 𝐭𝐨 𝐩𝐫𝐨𝐯𝐢𝐝𝐞 𝐅𝐞𝐞𝐝𝐛𝐚𝐜𝐤 𝐛𝐲 𝐭𝐚𝐩𝐩𝐢𝐧𝐠 𝐭𝐡𝐢𝐬 𝐔𝐑𝐋 https://g.page/r/CZGVC78LPsb0EBE/review, 𝐈𝐭 𝐭𝐚𝐤𝐞𝐬 𝐭𝐰𝐨 𝐦𝐢𝐧𝐮𝐭𝐞𝐬 𝐨𝐟 𝐲𝐨𝐮𝐫 𝐭𝐢𝐦𝐞 𝐚𝐧𝐝 𝐡𝐞𝐥𝐩𝐬 𝐮𝐬 𝐭𝐨 𝐄𝐧𝐜𝐨𝐮𝐫𝐚𝐠𝐞 𝐚𝐧𝐝 𝐢𝐦𝐩𝐫𝐨𝐯𝐞 𝐨𝐮𝐫 𝐬𝐞𝐫𝐯𝐢𝐜𝐞𝐬. 
+
+________________________________________
+              "𝐌𝐮𝐬𝐢𝐜 𝐤𝐞𝐞𝐩𝐬 𝐬𝐨𝐮𝐥 𝐜𝐚𝐥𝐦."
+        "𝐊𝐞𝐞𝐩 𝐏𝐫𝐚𝐜𝐭𝐢𝐜𝐢𝐧𝐠 𝐊𝐞𝐞𝐩 𝐑𝐨𝐜𝐤𝐢𝐧𝐠"
+________________________________________
+
+👋 𝐄𝐱𝐜𝐢𝐭𝐢𝐧𝐠 𝐍𝐞𝐰𝐬! 𝐀𝐝𝐦𝐢𝐬𝐬𝐢𝐨𝐧 𝐎𝐩𝐞𝐧 📢
+       - 🔘 𝔹𝕠𝕠𝕜 𝕐𝕠𝕦𝕣 𝕊𝕡𝕠𝕥 ℕ𝕠𝕨 🔘
+
+📝 𝐑𝐞𝐠𝐢𝐬𝐭𝐫𝐚𝐭𝐢𝐨𝐧 𝐅𝐨𝐫 :-
+       - 🎶 𝕄𝕦𝕤𝕚𝕔
+       - 🎭 𝔸𝕔𝕥𝕚𝕟𝕘 
+       - 🕺 𝔻𝕒𝕟𝕔𝕖
+
+🎶 𝐃𝐢𝐯𝐞 𝐢𝐧𝐭𝐨 𝐕𝐚𝐫𝐢𝐨𝐮𝐬 𝐆𝐞𝐧𝐫𝐞𝐬 :-
+       - 🎵 ℍ𝕚𝕟𝕕𝕦𝕤𝕥𝕒𝕟𝕚
+       - 🎻 ℂ𝕝𝕒𝕤𝕤𝕚𝕔𝕒𝕝 
+       - 🎸 𝕎𝕖𝕤𝕥𝕖𝕣𝕟
+
+🔆 𝐅𝐥𝐞𝐱𝐢𝐛𝐥𝐞 𝐋𝐞𝐚𝐫𝐧𝐢𝐧𝐠 𝐌𝐨𝐝𝐞𝐬 :-
+       - 🏫 𝕆𝕗𝕗𝕝𝕚𝕟𝕖 ℂ𝕝𝕒𝕤𝕤𝕖𝕤
+       - 🌐 𝕆𝕟𝕝𝕚𝕟𝕖 𝕊𝕖𝕤𝕤𝕚𝕠𝕟𝕤
+       - 🌟 ℙ𝕣𝕚𝕧𝕒𝕥𝕖 ℂ𝕠𝕒𝕔𝕙𝕚𝕟𝕘
+
+📆 𝐉𝐨𝐢𝐧 𝟑 𝐖𝐞𝐞𝐤𝐥𝐲 𝐒𝐞𝐬𝐬𝐢𝐨𝐧𝐬
+
+🌟 𝐄𝐱𝐩𝐥𝐨𝐫𝐞 𝐓𝐡𝐫𝐞𝐞 𝐋𝐞𝐯𝐞𝐥𝐬 :-
+{ 𝐁𝐞𝐠𝐢𝐧𝐧𝐞𝐫 / 𝐈𝐧𝐭𝐞𝐫𝐦𝐞𝐝𝐢𝐚𝐭𝐞 / 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 𝐓𝐞𝐜𝐡𝐧𝐢𝐪𝐮𝐞𝐬 }
+
+🏫 𝐋𝐞𝐚𝐫𝐧 𝐈𝐧𝐬𝐭𝐫𝐮𝐦𝐞𝐧𝐭𝐬 :-
+       - 🎸 𝔾𝕦𝕚𝕥𝕒𝕣 
+       - 🎹 𝕂𝕖𝕪𝕓𝕠𝕒𝕣𝕕 (ℙ𝕚𝕒𝕟𝕠)
+       - 🥁 𝔻𝕣𝕦𝕞 𝕊𝕖𝕥
+       - 🪈 𝔽𝕝𝕦𝕥𝕖
+(🪘𝕋𝕒𝕓𝕝𝕒, 𝔻𝕙𝕠𝕝𝕒𝕜, 𝔻𝕙𝕠𝕝𝕒𝕜𝕚, ℍ𝕒𝕣𝕞𝕠𝕟𝕚𝕦𝕞, & 𝕄𝕒𝕟𝕪 𝕞𝕠𝕣𝕖 )
+
+________________________________________
+                    🤗 𝐓𝐇𝐀𝐍𝐊𝐒 !
+   
+       "𝐓𝐫𝐚𝐧𝐬𝐟𝐨𝐫𝐦𝐢𝐧𝐠 𝐩𝐚𝐬𝐬𝐢𝐨𝐧 𝐢𝐧𝐭𝐨 𝐦𝐞𝐥𝐨𝐝𝐲.
+          𝐊𝐞𝐞𝐩 𝐩𝐥𝐚𝐲𝐢𝐧𝐠, 𝐤𝐞𝐞𝐩 𝐩𝐫𝐨𝐠𝐫𝐞𝐬𝐬𝐢𝐧𝐠."
+________________________________________
+
+🔗 𝐂𝐨𝐧𝐧𝐞𝐜𝐭 𝐰𝐢𝐭𝐡 𝐔𝐬 :-
+              *- ☎️ 7775077248*
+              *- 📱 9595618414*
+
+📍𝐀𝐝𝐝𝐫𝐞𝐬𝐬 𝐅𝐨𝐫 𝐎𝐟𝐟𝐥𝐢𝐧𝐞 𝐂𝐥𝐚𝐬𝐬𝐞𝐬 :-   
+                    🏫 ℙ𝕌ℕ𝔼 ℂ𝕀𝕋𝕐 & 
+       ℂ𝕙𝕙. 𝕊𝔸𝕄𝔹ℍ𝔸𝕁𝕀ℕ𝔸𝔾𝔸ℝ ℂ𝕀𝕋𝕐
+
+
+
+
+
+
+Hello, I want to know more about 7th Chords Academy !
+`;
+
+  window.open(`https://api.whatsapp.com/send/?phone=919595618414&text=${encodeURIComponent(message)}`, "_blank");
+};
+
+
   return (
     <section id="contact" className="bg-gray-50 py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-6xl mx-auto text-center mb-12">
@@ -412,14 +510,13 @@ const Registration = () => {
               </a>
 
               {/* WhatsApp Button */}
-              <a
-                href="https://q.me-qr.com/WW0Bheb7"
-                target="_blank"
+              <button
+                onClick={handleWhatsAppRedirect}
                 rel="noopener noreferrer"
                 className="flex-1 flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition-colors duration-300"
               >
                 <MessageCircleHeart className="mr-2" size={20} /> WhatsApp
-              </a>
+              </button>
             </div>
 
             <div className="relative z-0 mt-8 h-64 w-full rounded-lg overflow-hidden shadow-lg">
